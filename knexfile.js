c33fcd9949588aa740edad0fca01986beec2cf5c
yanipo5 <[yip528@gmail.com]>
2018-01-27 15:08:39 +0200

    update DB tabele name

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
       host: '127.0.0.1',
       user: 'root',
       password: 'root',
-      database: 'test-knex'
+      database: 'MenuSystemDB'
     },
     migrations: {
       directory: `${__dirname}/db/migrations`
