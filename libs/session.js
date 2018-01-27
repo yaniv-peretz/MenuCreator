d102b80763954323627a17f66098887f6abc71f4
yanipo5 <[yip528@gmail.com]>
2018-01-27 15:08:28 +0200

    update session secret

--- libs/session.js
+++ libs/session.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 module.exports = app => {
     app.set('trust proxy', 1) // trust first proxy
     app.use(session({
-        secret: 'a',
+        secret: process.env.SESSION_SECRECT || 'testSecrect',
         resave: false,
         saveUninitialized: true,
         cookie: {
