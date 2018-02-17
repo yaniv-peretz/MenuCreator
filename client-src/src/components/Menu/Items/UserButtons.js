c4627c3d495f62541892f23705a1d411d33a34b0
yanipo5 <[yip528@gmail.com]>
2018-02-17 12:59:07 +0200

    button fixers

--- client-src/src/components/Menu/Items/UserButtons.js
+++ client-src/src/components/Menu/Items/UserButtons.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { observer } from "mobx-react";
+import { Edit, Trash, Plus } from "react-bytesize-icons";
 
 @observer
 class UserButtons extends Component {
@@ -11,19 +12,32 @@ class UserButtons extends Component {
         <div className="User-Buttons">
           <button
             type="button"
-            className="add-button"
+            className="btn btn-success"
             onClick={() => store.addItem(this.props.index)}
-          />
+          >
+            <span className="inner-icon">
+              <Plus width={"100%"} height={"100%"} />
+            </span>
+          </button>
+
           <button
             type="button"
-            className="edit-button"
+            className="btn btn-warning "
             onClick={() => this.props.toggleEditMode()}
-          />
+          >
+            <span className="inner-icon">
+              <Edit width={"100%"} height={"100%"} />
+            </span>
+          </button>
           <button
             type="button"
-            className="remove-button"
+            className="btn btn-danger "
             onClick={() => store.deleteItem(props.id, props.index)}
-          />
+          >
+            <span className="inner-icon">
+              <Trash width={"100%"} height={"100%"} />
+            </span>
+          </button>
         </div>
       );
     } else {
